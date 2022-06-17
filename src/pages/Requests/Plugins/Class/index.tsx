ea83cbdf382b1e695c5bdd4cdec0b87fdbb198fe
GuilhermeSantosUI <guilherme.contato080@gmail.com>
2022-06-17 03:06:59 -0300

    forgot to add class plugin request page

---
+++ src/pages/Requests/Plugins/Class/index.tsx
@@ -0,0 +1,12 @@
+import HeaderSection from '../../../../components/BasePage/HeaderSection';
+import * as C from '../styles';
+
+function Class() {
+  return (
+    <C.SectionContainer>
+      <HeaderSection title="Turmas" subtitle="Coordenadoria de eletrônica" />
+    </C.SectionContainer>
+  );
+}
+
+export default Class;
