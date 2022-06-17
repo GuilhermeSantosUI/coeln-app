2c0a2a29776be03392ac872bd776215e8b410d7d
GuilhermeSantosUI <guilherme.contato080@gmail.com>
2022-06-17 03:06:37 -0300

    creating plugins for request pages

---
+++ src/pages/Requests/Plugins/Course/index.tsx
@@ -0,0 +1,12 @@
+import HeaderSection from '../../../../components/BasePage/HeaderSection';
+import * as C from '../styles';
+
+function Course() {
+  return (
+    <C.SectionContainer>
+      <HeaderSection title="Cursos" subtitle="Coordenadoria de eletrônica" />
+    </C.SectionContainer>
+  );
+}
+
+export default Course;
---
+++ src/pages/Requests/Plugins/index.tsx
@@ -0,0 +1,44 @@
+import Accordion from '../../../components/Accordion';
+import {
+  Aside,
+  AsideTitle,
+  MainContent,
+  Section,
+} from '../../../components/BasePage';
+import HeaderSection from '../../../components/BasePage/HeaderSection';
+import Header from '../../../components/Header';
+import Goback from '../../../components/Sidebar/Goback';
+import { FlowSection } from '../../MainPage/styles';
+import * as C from './styles';
+
+function RequestPlugins() {
+  return (
+    <C.Container>
+      <Header />
+
+      <MainContent>
+        <Goback />
+
+        <FlowSection>
+          <Aside style={{ borderLeft: '1px solid #f2f2f2' }}>
+            <AsideTitle>
+              Mais informações <br />
+              para detalhação de items:
+            </AsideTitle>
+
+            <Accordion />
+          </Aside>
+
+          <Section>
+            <HeaderSection
+              title="Observações"
+              subtitle="Coordenadoria de eletrônica"
+            />
+          </Section>
+        </FlowSection>
+      </MainContent>
+    </C.Container>
+  );
+}
+
+export default RequestPlugins;
