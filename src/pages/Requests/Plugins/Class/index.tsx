d7803b330b39fc9116626f4b28e98e07ebf2b94d
GuilhermeSantosUI <guilherme.contato080@gmail.com>
2022-06-18 17:46:51 -0300

    adding list of class and course in request plugins

--- src/pages/Requests/Plugins/Class/index.tsx
+++ src/pages/Requests/Plugins/Class/index.tsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from 'react';
+import { FiEdit3, FiTrash } from 'react-icons/fi';
+import { Separator } from '../../../../components/BasePage';
 import HeaderSection from '../../../../components/BasePage/HeaderSection';
+import {
+  AvatarContainer,
+  ListContainer,
+  Subtitle,
+  Title,
+} from '../../../../components/BasePage/ListComponents';
+import OptionButton from '../../../../components/OptionButton';
+import handleRandomAvatar from '../../../../components/RandomImage';
+import api from '../../../../services/api';
 import * as C from '../styles';
 
 function Class() {
+  const [classes, setClasses] = useState<any[]>([]);
+
+  useEffect(() => {
+    (async function handleGet() {
+      const { data } = await api.get('/turmas');
+      setClasses(data);
+    })();
+  }, []);
+
   return (
     <C.SectionContainer>
       <HeaderSection title="Turmas" subtitle="Coordenadoria de eletrônica" />
+
+      {classes.map((classData: any) => (
+        <ListContainer key={classData.id}>
+          <AvatarContainer>
+            {handleRandomAvatar()}
+
+            <Separator>
+              <Subtitle>Período: </Subtitle>
+              <Title>{classData?.nome}</Title>
+            </Separator>
+          </AvatarContainer>
+
+          <Separator>
+            <OptionButton>
+              <FiEdit3 size={20} color="#8c8c8c" />
+            </OptionButton>
+
+            <OptionButton>
+              <FiTrash size={20} color="#8c8c8c" />
+            </OptionButton>
+          </Separator>
+        </ListContainer>
+      ))}
     </C.SectionContainer>
   );
 }
--- src/pages/Requests/Plugins/Course/index.tsx
+++ src/pages/Requests/Plugins/Course/index.tsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from 'react';
+import { FiEdit3, FiTrash } from 'react-icons/fi';
+import { Separator } from '../../../../components/BasePage';
 import HeaderSection from '../../../../components/BasePage/HeaderSection';
+import {
+  AvatarContainer,
+  ListContainer,
+  Subtitle,
+  Title,
+} from '../../../../components/BasePage/ListComponents';
+import OptionButton from '../../../../components/OptionButton';
+import handleRandomAvatar from '../../../../components/RandomImage';
+import api from '../../../../services/api';
 import * as C from '../styles';
 
 function Course() {
+  const [courses, setCourses] = useState<any[]>([]);
+
+  useEffect(() => {
+    (async function handleGet() {
+      const { data } = await api.get('/cursos');
+      setCourses(data);
+    })();
+  }, []);
+
   return (
     <C.SectionContainer>
       <HeaderSection title="Cursos" subtitle="Coordenadoria de eletrônica" />
+
+      {courses.map((course: any) => (
+        <ListContainer key={course.id}>
+          <AvatarContainer>
+            {handleRandomAvatar()}
+
+            <Separator>
+              <Subtitle>Nome: </Subtitle>
+              <Title>{course?.nome}</Title>
+            </Separator>
+          </AvatarContainer>
+
+          <Separator>
+            <OptionButton>
+              <FiEdit3 size={20} color="#8c8c8c" />
+            </OptionButton>
+
+            <OptionButton>
+              <FiTrash size={20} color="#8c8c8c" />
+            </OptionButton>
+          </Separator>
+        </ListContainer>
+      ))}
     </C.SectionContainer>
   );
 }
