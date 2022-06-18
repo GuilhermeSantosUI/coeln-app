409cff8e71da53f256e0a99661296c3b0df934ff
GuilhermeSantosUI <guilherme.contato080@gmail.com>
2022-06-18 17:36:58 -0300

    adding list of notes, types and organizers to plugins page

--- src/pages/Items/Plugins/Note/index.tsx
+++ src/pages/Items/Plugins/Note/index.tsx
@@ -1,13 +1,57 @@
+import { useEffect, useState } from 'react';
+import { FiEdit3, FiTrash } from 'react-icons/fi';
+import { Separator } from '../../../../components/BasePage';
 import HeaderSection from '../../../../components/BasePage/HeaderSection';
+import {
+  AvatarContainer,
+  ListContainer,
+  Subtitle,
+  Title,
+} from '../../../../components/BasePage/ListComponents';
+import OptionButton from '../../../../components/OptionButton';
+import handleRandomAvatar from '../../../../components/RandomImage';
+import api from '../../../../services/api';
 import * as C from '../styles';
 
 function Note() {
+  const [notes, setNotes] = useState<any[]>([]);
+
+  useEffect(() => {
+    (async function handleGet() {
+      const { data } = await api.get('/observacoes');
+      setNotes(data);
+    })();
+  }, []);
+
   return (
     <C.SectionContainer>
       <HeaderSection
         title="Observações"
         subtitle="Coordenadoria de eletrônica"
       />
+
+      {notes.map((note: any) => (
+        <ListContainer>
+          <AvatarContainer>
+            {handleRandomAvatar()}
+
+            <Separator>
+              <Subtitle>Texto: </Subtitle>
+              <Title>{note?.texto}</Title>
+            </Separator>
+          </AvatarContainer>
+
+          <Separator>
+            <OptionButton>
+              <FiEdit3 size={20} color="#8c8c8c" />
+            </OptionButton>
+
+            <OptionButton>
+              <FiTrash size={20} color="#8c8c8c" />
+            </OptionButton>
+          </Separator>
+        </ListContainer>
+      ))}
     </C.SectionContainer>
   );
 }
--- src/pages/Items/Plugins/Type/index.tsx
+++ src/pages/Items/Plugins/Type/index.tsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from 'react';
+import { FiEdit3, FiTrash } from 'react-icons/fi';
+import { Separator } from '../../../../components/BasePage';
 import HeaderSection from '../../../../components/BasePage/HeaderSection';
+import {
+  AvatarContainer,
+  ListContainer,
+  Subtitle,
+  Title,
+} from '../../../../components/BasePage/ListComponents';
+import OptionButton from '../../../../components/OptionButton';
+import handleRandomAvatar from '../../../../components/RandomImage';
+import api from '../../../../services/api';
 import * as C from '../styles';
 
 function Type() {
+  const [types, setTypes] = useState<any[]>([]);
+
+  useEffect(() => {
+    (async function handleGet() {
+      const { data } = await api.get('/tipos');
+      setTypes(data);
+    })();
+  }, []);
+
   return (
     <C.SectionContainer>
       <HeaderSection title="Tipos" subtitle="Coordenadoria de eletrônica" />
+
+      {types.map((type: any) => (
+        <ListContainer>
+          <AvatarContainer>
+            {handleRandomAvatar()}
+
+            <Separator>
+              <Subtitle>Categoria: </Subtitle>
+              <Title>{type?.nome}</Title>
+            </Separator>
+          </AvatarContainer>
+
+          <Separator>
+            <OptionButton>
+              <FiEdit3 size={20} color="#8c8c8c" />
+            </OptionButton>
+
+            <OptionButton>
+              <FiTrash size={20} color="#8c8c8c" />
+            </OptionButton>
+          </Separator>
+        </ListContainer>
+      ))}
     </C.SectionContainer>
   );
 }
