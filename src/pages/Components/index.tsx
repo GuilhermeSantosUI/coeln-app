f31810ce8d77bf03a489882ed0ccfea77cdeb9cc
GuilhermeSantosUI <guilherme.contato080@gmail.com>
2022-06-18 15:34:19 -0300

    list components in component page

--- src/pages/Components/index.tsx
+++ src/pages/Components/index.tsx
@@ -1,9 +1,12 @@
+import { FiArrowRight } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   Aside,
   AsideTitle,
   MainContent,
   Section,
+  Separator,
 } from '../../components/BasePage';
 import HeaderSection from '../../components/BasePage/HeaderSection';
 import Header from '../../components/Header';
@@ -12,9 +15,27 @@ import { FlowSection } from '../MainPage/styles';
 import Accordion from '../../components/Accordion';
 import * as C from './styles';
 import dataItemPlugin from '../Items/Plugins/data-item-plugin';
+import api from '../../services/api';
+import {
+  AvatarContainer,
+  ListContainer,
+  Subtitle,
+  Title,
+} from '../../components/BasePage/ListComponents';
+import handleRandomAvatar from '../../components/RandomImage';
+import OptionButton from '../../components/OptionButton';
 
 function Components() {
   const navigate = useNavigate();
+
+  const [components, setComponents] = useState<any[]>([]);
+
+  useEffect(() => {
+    (async function handleGet() {
+      const { data } = await api.get('/componentes');
+      setComponents(data);
+    })();
+  }, []);
 
   function handleGetValue(e: string) {
     navigate(`/item/plugins/${e}`);
@@ -33,6 +54,39 @@ function Components() {
               title="Componentes"
               subtitle="Coordenadoria de eletrônica"
             />
+
+            {components.map((component) => (
+              <ListContainer key={component.id}>
+                <AvatarContainer>
+                  {handleRandomAvatar()}
+
+                  <Separator>
+                    <Subtitle>Tipo: {component.tipo.nome}</Subtitle>
+                    <Title>{component.nome}</Title>
+                  </Separator>
+                </AvatarContainer>
+
+                <C.UnityComponent>
+                  {component.observacoes.length !== 0 ? (
+                    <Separator>
+                      <Subtitle>Verifique as observações</Subtitle>
+                      <Title>
+                        {component.observacoes.length}{' '}
+                        {component.observacoes.length === 1
+                          ? 'observação'
+                          : 'observações'}
+                      </Title>
+                    </Separator>
+                  ) : (
+                    <Title>Não há observações</Title>
+                  )}
+
+                  <OptionButton>
+                    <FiArrowRight size={20} color="#8c8c8c" />
+                  </OptionButton>
+                </C.UnityComponent>
+              </ListContainer>
+            ))}
           </Section>
 
           <Aside>
