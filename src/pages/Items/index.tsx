6eea7ee22b904636a82ce50838110d946b553615
GuilhermeSantosUI <guilherme.contato080@gmail.com>
2022-06-18 15:34:38 -0300

    adding navigate button in items section

--- src/pages/Items/index.tsx
+++ src/pages/Items/index.tsx
@@ -1,10 +1,19 @@
+import { FiCpu } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
 import HeaderSection from '../../components/BasePage/HeaderSection';
+import OptionButton from '../../components/OptionButton';
 import * as C from '../MainPage/styles';
 
 function Items() {
+  const navigate = useNavigate();
+
   return (
     <C.SectionContainer>
-      <HeaderSection title="Items" subtitle="Coordenadoria de eletrônica" />
+      <HeaderSection title="Items" subtitle="Coordenadoria de eletrônica">
+        <OptionButton onClick={() => navigate('/components')}>
+          <FiCpu size={20} color="#8C8C8C" />
+        </OptionButton>
+      </HeaderSection>
     </C.SectionContainer>
   );
 }
