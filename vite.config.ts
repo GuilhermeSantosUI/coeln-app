8b28a0902d462442aaf1f1b051bb5877b9adf5fa
GuilhermeSantosUI <guilherme.contato080@gmail.com>
2023-04-21 15:36:01 -0300

    config: adding import settings to root path

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@
 import { configDefaults, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
+import { resolve } from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+    '@': resolve(__dirname, './src'),
+    "#root": resolve(__dirname),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
