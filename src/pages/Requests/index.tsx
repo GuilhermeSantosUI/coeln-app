79d12c2c4facde416d2cb2441df81f9c845ebfd6
“guiherme” <guilherme@agapesistemas.com.br>
2024-11-18 16:25:58 -0300

    feat: add loading skeleton for requests list

--- src/pages/Requests/index.tsx
+++ src/pages/Requests/index.tsx
@@ -19,15 +19,22 @@ import api from '@/services/api';
 import OptionButton from '@/components/OptionButton';
 import handleRandomAvatar from '@/components/RandomImage';
 import { SectionContainer } from '../MainPage/styles';
+import { SkeletonContainer } from '../Students/styles';
+import { SkeletonItem } from '../../components/Skeleton';
 
 function Requests() {
   const navigate = useNavigate();
   const [requests, setRequests] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     (async function handleGet() {
+      setIsLoading(true);
+
       const { data } = await api.get('/pedidos');
       setRequests(data);
+
+      setIsLoading(false);
     })();
   }, []);
 
@@ -47,39 +54,67 @@ function Requests() {
         </HeaderSeparator>
       </HeaderSection>
 
-      {requests.map((request) => (
-        <ListContainer
-          key={request.id}
-          onClick={() => navigate(`/request/${request.id}`)}>
-          <AvatarContainer>
-            {handleRandomAvatar()}
-            <Separator>
-              <Subtitle>Pedinte</Subtitle>
-              <Title>{request.usuario.nome}</Title>
-            </Separator>
-          </AvatarContainer>
+      {isLoading ? (
+        <SkeletonContainer>
+          <SkeletonItem
+            width="100%"
+            height="70px"
+            style={{ border: '1px solid #ffffff' }}
+          />
 
-          {request.data_devolucao ? (
-            <RightSeparator>
-              <>
-                <Button loading={false} colorStyle="tined" size="small">
-                  {moment(request?.data_entregue).format('DD/MM/YYYY')}
-                </Button>
+          <SkeletonItem
+            width="100%"
+            height="70px"
+            style={{ border: '1px solid #ffffff' }}
+          />
 
-                <FiArrowRight size={20} color="#8c8c8c" />
+          <SkeletonItem
+            width="100%"
+            height="70px"
+            style={{ border: '1px solid #ffffff' }}
+          />
 
-                <Button loading={false} colorStyle="filled" size="small">
-                  {moment(request?.data_devolucao).format('DD/MM/YYYY')}
-                </Button>
-              </>
-            </RightSeparator>
-          ) : (
-            <Button loading={false} colorStyle="tined" size="small">
-              Sem previsão
-            </Button>
-          )}
-        </ListContainer>
-      ))}
+          <SkeletonItem
+            width="100%"
+            height="70px"
+            style={{ border: '1px solid #ffffff' }}
+          />
+        </SkeletonContainer>
+      ) : (
+        requests.map((request) => (
+          <ListContainer
+            key={request.id}
+            onClick={() => navigate(`/request/${request.id}`)}>
+            <AvatarContainer>
+              {handleRandomAvatar()}
+              <Separator>
+                <Subtitle>Pedinte</Subtitle>
+                <Title>{request.usuario.nome}</Title>
+              </Separator>
+            </AvatarContainer>
+
+            {request.data_devolucao ? (
+              <RightSeparator>
+                <>
+                  <Button loading={false} colorStyle="tined" size="small">
+                    {moment(request?.data_entregue).format('DD/MM/YYYY')}
+                  </Button>
+
+                  <FiArrowRight size={20} color="#8c8c8c" />
+
+                  <Button loading={false} colorStyle="filled" size="small">
+                    {moment(request?.data_devolucao).format('DD/MM/YYYY')}
+                  </Button>
+                </>
+              </RightSeparator>
+            ) : (
+              <Button loading={false} colorStyle="tined" size="small">
+                Sem previsão
+              </Button>
+            )}
+          </ListContainer>
+        ))
+      )}
     </SectionContainer>
   );
 }
